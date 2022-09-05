9b4657e792de9b377e3f3aa3da2a00ceee9f3963
ukcastle <jo631@naver.com>
2022-09-05 13:38:11 +0900

    init

---
+++ onnx_test/Onnx_MMpose.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenCvSharp;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace onnx_test
+{
+    class Onnx_MMpose
+    {
+        private static readonly int[,] skeleton = new int[17, 5]
+        {
+            { 15, 13, 255, 128, 0 }, // Left Ankle - Left Knee
+            { 16, 14, 255, 128, 0 }, // Right Ankle - Right Knee
+            { 11, 13, 255, 0, 0 }, // Left Knee - Left Hip
+            { 12, 14, 255, 0, 0 }, // Right Knee - Right Hip
+            { 11, 12, 255, 255, 0 }, // Left Hip - Right Hip
+            { 5, 11, 0, 255, 0 }, // Left Shoulder - Left Hip
+            { 6, 12, 0, 255, 0 }, // Right Shoulder - Right Hip
+            { 5, 7, 0, 255, 255 }, // Left Shoulder - Left Elbow
+            { 6, 8, 0, 255, 255 }, // Right Shoulder - Right Elbow
+            { 9, 7, 255, 0, 255 }, // Left Wrist - Left Elbow
+            { 10, 8, 255, 0, 255 }, // Right Wrist - Right Elbow
+            { 5, 1, 128, 128, 255 }, // Left Shoulder - Head Bottom
+            { 6, 1, 128, 128, 255 }, // Right Shoulder - Head Bottom
+            { 0, 1, 128, 128, 128 }, // Nose - Head Bottom
+            { 0, 2, 128, 128, 128 }, // Nose - Head Top
+            { 0, 3, 0, 0, 128 }, // Nose - Left Ear
+            { 0, 4, 0, 0, 128 } // Nose - Right Ear
+        };
+        private enum KeyPoint
+        {
+            Nose = 0,
+            HeadBottom,
+            HeadTop,
+            LeftEar,
+            RightEar,
+            LeftShoulder,
+            RightShoulder,
+            LeftElbow,
+            RightElbow,
+            LeftWrist,
+            RightWrist,
+            LeftHip,
+            RightHip,
+            LeftKnee,
+            RightKnee,
+            LeftAnkle,
+            RightAnkle
+        }
+        private readonly InferenceSession sess;
+        private Size size;
+        private Scalar backgroundColor;
+
+        public Onnx_MMpose(string onnxPath, int width, int height, int color = 114)
+        {
+            if (string.IsNullOrWhiteSpace(onnxPath))
+            {
+                throw new ArgumentException("message", nameof(onnxPath));
+            }
+
+            var option = new SessionOptions
+            {
+                GraphOptimizationLevel = GraphOptimizationLevel.ORT_ENABLE_ALL,
+                ExecutionMode = ExecutionMode.ORT_SEQUENTIAL
+            };
+            
+            this.sess = new InferenceSession(onnxPath, option);
+            this.size = new Size(width, height);
+            this.backgroundColor = new Scalar(color, color, color);
+        }
+        
+        public DisposableNamedOnnxValue[] ModelRun(List<NamedOnnxValue> input)
+        {
+            return this.sess.Run(input).ToArray();
+        }
+
+
+        public Mat MakeLetterBoxByMat(ref Mat input, out float ratio, out Point diff, out Point diff2, bool auto = true, bool scaleFill = false, bool isScaleUp = true)
+        {
+            Mat img = input.Clone();
+            //Cv2.CvtColor(img, img, ColorConversionCodes.RGB2BGR);
+            ratio = Math.Min((float)this.size.Width / img.Width, (float)this.size.Height / img.Height);
+
+            if (!isScaleUp)
+            {
+                ratio = Math.Min(ratio, 1.0f);
+            }
+
+            var unpad = new Size((int)Math.Round(img.Width * ratio), (int)Math.Round(img.Height * ratio));
+
+            var dW = this.size.Width - unpad.Width;
+            var dH = this.size.Height - unpad.Height;
+
+            var tensorRatio = this.size.Height / (float)this.size.Width;
+            var inputRatio = input.Height / (float)input.Width;
+
+            if (auto && tensorRatio != inputRatio)
+            {
+                dW %= 32;
+                dH %= 32;
+            }
+            else if (scaleFill)
+            {
+                dW = 0;
+                dH = 0;
+                unpad = this.size;
+            }
+
+            var dW_h = (int)Math.Round((float)dW / 2);
+            var dH_h = (int)Math.Round((float)dH / 2);
+            var dw2 = 0;
+            var dh2 = 0;
+            if (dW_h * 2 != dW)
+            {
+                dw2 = dW - dW_h * 2;
+            }
+            if (dH_h * 2 != dH)
+            {
+                dh2 = dH - dH_h * 2;
+            }
+
+            if (img.Width != unpad.Width || img.Height != unpad.Height)
+            {
+                Cv2.Resize(img, img, unpad);
+            }
+            Cv2.CopyMakeBorder(img, img, dH_h + dh2, dH_h,
+                               dW_h + dw2, dW_h, BorderTypes.Constant, this.backgroundColor);
+            diff = new Point(dW_h, dH_h);
+            diff2 = new Point(dw2, dh2);
+            return img;
+        }
+    }
+}
---
+++ onnx_test/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+
+namespace onnx_test
+{
+    static class Program
+    {
+        
+        [STAThread]
+        static void Main()
+        {
+            int[,] skeleton = new int[17, 5]
+            {
+                {15, 13, 255, 128, 0 },
+                {16, 14, 255, 128, 0 },
+                {12, 14, 255, 0, 0 },
+                {11, 13, 255, 0, 0 },
+                {11, 12, 255, 255, 0 },
+                {6, 12, 0, 255, 0 },
+                {5, 11, 0, 255, 0 },
+                {6, 8, 0, 255, 255 },
+                {5, 7, 0, 255, 255 },
+                {10, 8, 255, 0, 255 },
+                {9, 7, 255, 0, 255 },
+                {6, 1, 128, 128, 255 },
+                {5, 1, 128, 128, 255 },
+                {0, 1, 128, 128, 128 },
+                {0, 2, 128, 128, 128 },
+                {0, 3, 0, 0, 128 },
+                {0, 4, 0, 0, 128 }
+            };
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Onnx_MMpose onnxPose = new Onnx_MMpose("C:\\Users\\Admin\\Documents\\output.onnx", 192, 256);
+
+            Mat src = Cv2.ImRead("C:\\Users\\Admin\\Documents\\3326.jpg", ImreadModes.Color);
+            var inputMat = onnxPose.MakeLetterBoxByMat(ref src, out float ratio, out Point diff, out Point diff2, auto:false, scaleFill:false);
+            Mat visMat = inputMat.Clone();
+        
+            inputMat.ConvertTo(inputMat, MatType.CV_32FC3, (float)(1 / 255.0)); // 아직 normalize 안했음 
+            var inputTensor = new DenseTensor<float>(Mat2Array(inputMat), new[] { 1, 3, inputMat.Height, inputMat.Width });
+
+
+            var onnxInput = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("input.1", inputTensor)
+            };
+
+            
+            var results = onnxPose.ModelRun(onnxInput); // 1(N) * 17(C) * 64(H) * 48(W)
+            
+
+            var predValue = results[0].AsEnumerable<float>().ToArray(); // 1 17 64 48
+            var predDims = results[0].AsTensor<float>().Dimensions.ToArray(); // 1 17 64 48
+
+            var output = PostProcessing(predValue, predDims, inputMat.Width, inputMat.Height);
+
+            for (int i=0; i<17; i++)
+            {
+                int startIdx = skeleton[i,0];
+                int endIdx = skeleton[i,1];
+                Scalar color = new Scalar(skeleton[i, 2], skeleton[i, 3], skeleton[i, 4]);
+
+                Point start = new Point(output[0][startIdx][0], output[0][startIdx][1]);
+                Point end = new Point(output[0][endIdx][0], output[0][endIdx][1]);
+
+                Cv2.Line(visMat, start, end, color);
+            }
+            Cv2.Line(visMat, diff, diff2, new Scalar(255,0,0));
+            Cv2.Resize(visMat, visMat, new Size(192 * 3, 256 * 3));
+            Cv2.ImShow("dd", visMat);
+            Cv2.WaitKey();
+        }
+
+        static List<List<List<float>>> PostProcessing(float[] predVlaue, int[] predDims, int imgWidth, int imgHegiht, int bboxX = 0, int bboxY = 0)
+        {
+            var keyPointsBatch = new List<List<List<float>>>(); // batch * 17(key) * 3(x,y,pred)
+
+            int scaleX = imgWidth / predDims[3];
+            int scaleY = imgHegiht / predDims[2];
+
+            for (int batch=0; batch < predDims[0]; batch++)
+            {
+                var keyPoints = new List<List<float>>(); // 17(key) * 3(x,y,pred)
+
+                for (int key=0; key<predDims[1]; key++)
+                {
+                    int idx1 = batch * predDims[1] * predDims[2] * predDims[3]; //사실 0만나옴
+                    int idx2 = key * predDims[2] * predDims[3];
+                    var keyValue = Heatmap2KeyPoint(predVlaue, idx1+idx2, predDims[2], predDims[3]); // 3(x,y,pred);
+                    refinePointBySize(ref keyValue, scaleX, scaleY, bboxX, bboxY);
+                    keyPoints.Add(keyValue);
+                }
+
+
+                keyPointsBatch.Add(keyPoints);
+            }
+
+            return keyPointsBatch;
+        }
+
+        static void refinePointBySize(ref List<float> keyValue, int scaleX, int scaleY, int bboxX, int bboxY)
+        {
+            keyValue[0] = keyValue[0] * scaleX + bboxX; // x
+            keyValue[1] = keyValue[1] * scaleY + bboxY; // y
+        }
+
+        static List<float> Heatmap2KeyPoint(float[] heatmap, int keyIdx, int heatmapHeight, int heatmapWidth)
+        {
+            var keyValue = new List<float>();
+            int topX = 0, topY = 0, absIdx = 0;
+            float topValue = 0.0f ;
+            for (int i = keyIdx; i < keyIdx + heatmapHeight * heatmapWidth; i++) // argmax, amax 로 좌표 저장
+            {
+                if (heatmap[i] > topValue)
+                {
+                    topValue = heatmap[i];
+                    absIdx = i;
+                    topX = (i - keyIdx) % heatmapWidth;
+                    topY = (i - keyIdx) / heatmapWidth;
+                }
+            }
+
+            keyValue.Add(RefinePoint(heatmap, topX, absIdx, heatmapHeight, heatmapWidth, isY: false));  // normalized x
+            keyValue.Add(RefinePoint(heatmap, topY, absIdx, heatmapHeight, heatmapWidth, isY: true));   // normalized y
+            keyValue.Add(topValue); // pred
+
+            return keyValue;
+        }
+
+        static float RefinePoint(float[] heatmap ,int point, int absIdx, int heatmapHeight, int heatmapWidth,  bool isY= false)
+        {
+            if ( (point < 1) || (!isY && point >= heatmapWidth - 1) || (isY && point >= heatmapHeight -1))
+            {
+                return point;
+            }
+
+            int step = isY ? heatmapWidth : 1;
+
+            float tempPoint = point;
+            
+            if ((heatmap[absIdx + step] - heatmap[absIdx - step]) >= 0)
+            {
+                tempPoint += 0.25f;
+            }
+            else
+            {
+                tempPoint -= 0.25f;
+            }
+
+
+            return tempPoint;
+
+        }
+
+        unsafe static float[] Mat2Array(Mat mat)
+        {
+            var imgHeight = mat.Height;
+            var imgWidth = mat.Width;
+            var imgChannel = mat.Channels();
+
+            float* matPointer = (float*)mat.DataPointer;
+
+            float[] array = new float[imgHeight * imgWidth * imgChannel]; // H * W * C
+
+            for (int y = 0; y < imgHeight; y++)
+            {
+                for (int x = 0; x < imgWidth; x++)
+                {
+                    for (int c = 0; c < imgChannel; c++)
+                    {
+                        var baseIdx = (y * imgChannel) * imgWidth + (x * imgChannel) + imgChannel;
+                        var convertedIdx = (c * imgWidth) * imgHeight + (y * imgWidth) + x;
+                        array[convertedIdx] = matPointer[baseIdx];
+                    }
+                }
+            }
+
+            return array;
+        } 
+
+        
+    }
+}
