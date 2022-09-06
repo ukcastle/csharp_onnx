333b415633090c8c849c19209ba57a4c03f44766
ukcastle <jo631@naver.com>
2022-09-06 09:30:44 +0900

    init other file

--- onnx_test/Program.cs
+++ onnx_test/Program.cs
@@ -21,7 +21,7 @@ namespace onnx_test
             //Mat src = Cv2.ImRead("C:\\Users\\Admin\\Documents\\3326.jpg", ImreadModes.Color);
             //var inputMat = onnxPose.MakeInputMat(ref src, out float ratio, out Point diff, out Point diff2, auto:false, scaleFill:false);
 
-            var inputMat = onnxPose.MakeInputMat("C:\\Users\\Admin\\Documents\\3326.jpg", out Mat src, out float ratio, out Point diff, out Point diff2);
+            var inputMat = onnxPose.MakeInputMat("C:\\Users\\Admin\\Documents\\20201123_General_001_DIS_S_F20_SS_001_0001.jpg", out Mat src, out float ratio, out Point diff, out Point diff2);
             var results = onnxPose.ModelRun(ref inputMat); // 1(N) * 17(C) * 64(H) * 48(W)
             var output = onnxPose.PostProcess(results, ref ratio, ref diff, ref diff2, inputMat.Width, inputMat.Height);
 
