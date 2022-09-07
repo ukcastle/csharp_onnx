5ed9ee766f9d050e98d9a00bf495812c9376515f
ukcastle <jo631@naver.com>
2022-09-07 17:58:35 +0900

    yolo inference

--- onnx_test/OnnxYolo.cs
+++ onnx_test/OnnxYolo.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.ML.OnnxRuntime;
 using OpenCvSharp;
+using OpenCvSharp.Dnn;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace onnx_test
@@ -14,18 +16,80 @@ namespace onnx_test
         private readonly float iouThresh;
 
 
-        public OnnxYolo(string onnxPath, float predThresh = 0.2f, float iouThresh = 0.4f, int width = 192, int height = 256, string inputName = "input.1")
+        public OnnxYolo(string onnxPath, float predThresh = 0.2f, float iouThresh = 0.4f, int width = 640, int height = 640, string inputName = "images")
             : base(onnxPath, width, height, inputName) {
             this.predThresh = predThresh;
             this.iouThresh = iouThresh;
         }
+
+
         public override List<List<float>> PostProcess(
             DisposableNamedOnnxValue[] output, ref float ratio, ref Point diff, int imgWidth, int imgHeight, int bboxX = 0, int bboxY = 0, int batchIdx = 0)
         {
             var predValue = output[0].AsEnumerable<float>().ToArray();
             var predDims = output[0].AsTensor<float>().Dimensions.ToArray();
 
-            var candidate = new List<List<float>>(); 
+            var candidate = this.GetCandidate(predValue, predDims, batchIdx);
+            Convert_xywh2xyxy(ref candidate);
+            var refinedCandidate = this.GetDetectionMatrix(candidate);
+            var nmsCandidate = this.NMS(refinedCandidate);
+            return FitSizeofOutput(ref nmsCandidate, ref ratio, ref diff);
+        }
+
+        public override Mat DrawOutput(ref Mat inputMat, List<List<float>> output)
+        {
+
+            var outputMat = inputMat.Clone();
+
+            for(int i = 0; i < output.Count; i++)
+            {
+                var x1 = (int)output[i][0];
+                var y1 = (int)output[i][1];
+                var x2 = (int)output[i][2];
+                var y2 = (int)output[i][3];
+
+                Cv2.Rectangle(outputMat, new Rect(x1, y1, x2 - x1, y2 - y1), new Scalar(255, 0, 0));
+            }
+            return outputMat;
+        }
+
+        protected override List<List<float>> FitSizeofOutput(ref List<List<float>> output, ref float ratio, ref Point diff)
+        {
+            var outputList = new List<List<float>>();
+
+            for (int i = 0; i < output.Count; i++)
+            {
+                float fitX1 = Math.Max(output[i][0] - diff.X, 0) / ratio;
+                float fitY1 = Math.Max(output[i][1] - diff.Y, 0) / ratio;
+                float fitX2 = Math.Max(output[i][2] - diff.X, 0) / ratio;
+                float fitY2 = Math.Max(output[i][3] - diff.Y, 0) / ratio;
+                output[i][0] = fitX1;
+                output[i][1] = fitY1;
+                output[i][2] = fitX2;
+                output[i][3] = fitY2;
+
+                outputList.Add(output[i]);
+            }
+
+            return outputList;
+        }
+        private void Convert_xywh2xyxy(ref List<List<float>> output)
+        {
+            for (int i = 0; i < output.Count; i++)
+            {
+                var x1 = output[i][0] - output[i][2] / 2;
+                var y1 = output[i][1] - output[i][3] / 2;
+                var x2 = output[i][0] + output[i][2] / 2;
+                var y2 = output[i][1] + output[i][3] / 2;
+                output[i][0] = x1;
+                output[i][1] = y1;  
+                output[i][2] = x2;
+                output[i][3] = y2;
+            }
+        }
+        private List<List<float>> GetCandidate(float[] predValue, int[] predDims, int batchIdx)
+        {
+            var candidate = new List<List<float>>();
 
             for (int preds = 0; preds < predDims[1]; preds++)
             {
@@ -33,52 +97,80 @@ namespace onnx_test
                 int predIdx = batch + preds * predDims[2];
 
                 var objectPred = predValue[predIdx + 4];
-                if(objectPred < this.predThresh) { continue; }
+                if (objectPred < this.predThresh) { continue; }
 
                 var curCandidate = new List<float>();
-                for (int i=0;i< 4; i++)
+                for (int i = 0; i < 4; i++)
                 {
-                    curCandidate.Add(predValue[predIdx+i]); // input x y w h  
+                    curCandidate.Add(predValue[predIdx + i]); // input x y w h  
                 }
 
                 curCandidate.Add(objectPred); // pred
 
-                for (int i=5; i < predDims[2]; i++)
+                for (int i = 5; i < predDims[2]; i++)
                 {
-                    curCandidate.Add(predValue[predIdx+i] * objectPred);
+                    curCandidate.Add(predValue[predIdx + i] * objectPred);
                 }
                 candidate.Add(curCandidate);
             }
 
-
-            //return this.FitSizeofOutput(ref keyPoints, ref ratio, ref diff);
+            return candidate;
         }
-
-
-        private List<List<float>> GetCandidate(float[] pred, int[] pred_dim)
+        private List<List<float>> GetDetectionMatrix(List<List<float>> candidate, int max_nms = 30000)
         {
-            List<List<float>> candidate = new List<List<float>>();
-            for (int batch = 0; batch < pred_dim[0]; batch++)
+            var mat = new List<List<float>>();
+            for (int i = 0; i < candidate.Count; i++)
             {
-                for (int cand = 0; cand < pred_dim[1]; cand++)
+                int cls = -1;
+                float max_score = 0;
+                for (int j = 5; j < candidate[i].Count; j++)
                 {
-                    int score = 4;  // objectness score
-                    int idx1 = (batch * pred_dim[1] * pred_dim[2]) + cand * pred_dim[2];
-                    int idx2 = idx1 + score;
-                    var value = pred[idx2];
-                    if (value > this.predThresh)
+                    if (candidate[i][j] > this.predThresh && candidate[i][j] >= max_score)
                     {
-                        List<float> tmp_value = new List<float>();
-                        for (int i = 0; i < pred_dim[2]; i++)
-                        {
-                            int sub_idx = idx1 + i;
-                            tmp_value.Add(pred[sub_idx]);
-                        }
-                        candidate.Add(tmp_value);
+                        cls = j;
+                        max_score = candidate[i][j];
                     }
                 }
+
+                if (cls < 0) continue;
+
+                List<float> tmpDetect = new List<float>();
+                for (int j = 0; j < 4; j++) tmpDetect.Add(candidate[i][j]); //box
+                tmpDetect.Add(candidate[i][cls]);   //class prob
+                tmpDetect.Add(cls - 5);             //class
+                mat.Add(tmpDetect);
             }
-            return candidate;
+
+            //max_nms sort
+            mat.Sort((a, b) => (a[4] > b[4]) ? -1 : 1);
+
+            if (mat.Count > max_nms)
+            {
+                mat.RemoveRange(max_nms, mat.Count - max_nms);
+            }
+            return mat;
+        }
+        private List<List<float>> NMS(List<List<float>> candidate, int maxWH = 640*12)
+        {
+            List<List<float>> nmsCandidate = new List<List<float>>();
+
+            List<Rect> bboxes = new List<Rect>();
+            List<float> confidences = new List<float>();
+
+            for (int i = 0; i < candidate.Count; i++)
+            {
+                var diff_class = (int)(maxWH * candidate[i][5]);
+
+                Rect box = new Rect((int)candidate[i][0] + diff_class, (int)candidate[i][1] + diff_class,
+                    (int)(candidate[i][2] - candidate[i][0]), (int)(candidate[i][3] - candidate[i][1]));
+                bboxes.Add(box);
+                confidences.Add(candidate[i][4]);
+            }
+            CvDnn.NMSBoxes(bboxes, confidences, this.predThresh, this.iouThresh, out int[] indices);
+
+            for (int i=0; i<indices.Length; i++) { nmsCandidate.Add(candidate[i]); }
+
+            return nmsCandidate;
         }
     }
 }
--- onnx_test/Program.cs
+++ onnx_test/Program.cs
@@ -11,38 +11,40 @@ namespace onnx_test
 {
     static class Program
     {
-        /*
-         * 100회 기준 tact time
-         * hr : 97ms, 10fps / 33ms, 30fps
-         * res50fp16 : 65ms, 15fps / 26ms, 38fps
-         * mobv2 : 48ms, 20fps /  14ms, 71fps
-         * shufflev2 : 33ms, 30fps / 16ms, 62fps
-         */
-         
         [STAThread]
         static void Main()
        {
             const string onnxPath = "C:\\Users\\Admin\\Documents\\hr.onnx";
+            const string yoloPath = "C:\\Users\\Admin\\Documents\\best.onnx";
             //const string imgPath = "C:\\Users\\Admin\\Documents\\3326.jpg";
             //const string imgPath = "C:\\Users\\Admin\\Documents\\20201123_General_001_DIS_S_F20_SS_001_0001.jpg";
+            const string imgPath = "C:\\Users\\Admin\\Documents\\20201202_General_068_DOS_A_M40_MM_007_0053.jpg";
             //const string imgPath = "C:\\Users\\Admin\\Documents\\aa1.jpg";
-            const string imgPath = "C:\\Users\\Admin\\Documents\\Capture_213.jpg";
             //const string imgPath = "C:\\Users\\Admin\\Documents\\1122.jpg";
+
+
+            OnnxYolo onnxYolo = new OnnxYolo(yoloPath);
+            var inputMat = onnxYolo.MakeInputMat(imgPath, out Mat src, out float ratio, out Point diff);
+            var results = onnxYolo.ModelRun(ref inputMat);
+            var output = onnxYolo.PostProcess(results, ref ratio, ref diff, inputMat.Width, inputMat.Height);
+            var dst = onnxYolo.DrawOutput(ref src, output);
+
 
             //float time = CheckTime(100, onnxPath, imgPath);
             //int fps = (int)(1000 / time);
 
-            OnnxMMpose onnxPose = new OnnxMMpose(onnxPath, 192, 256);
-            
-            var inputMat = onnxPose.MakeInputMat(imgPath, out Mat src, out float ratio, out Point diff);
-            var results = onnxPose.ModelRun(ref inputMat); // 1(N) * 17(C) * 64(H) * 48(W)
-            var output = onnxPose.PostProcess(results, ref ratio, ref diff, inputMat.Width, inputMat.Height); // 17(Key Point) * 3 (x, y, pred)
+            //OnnxMMpose onnxPose = new OnnxMMpose(onnxPath, 192, 256);
 
-            var dst = onnxPose.DrawOutput(ref src, output);
-            
+            //var inputMat = onnxPose.MakeInputMat(imgPath, out Mat src, out float ratio, out Point diff);
+            //var results = onnxPose.ModelRun(ref inputMat); // 1(N) * 17(C) * 64(H) * 48(W)
+            //var output = onnxPose.PostProcess(results, ref ratio, ref diff, inputMat.Width, inputMat.Height); // 17(Key Point) * 3 (x, y, pred)
+
+            //var dst = onnxPose.DrawOutput(ref src, output);
+
             Cv2.ImShow("dd", dst);
             Cv2.WaitKey();
             Cv2.DestroyAllWindows();
+
         }
 
         static float CheckTime(int cnt, string onnxPath, string imgPath)
